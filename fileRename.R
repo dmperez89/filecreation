c3aca8c418cf3a58ab12649ac5f5ff3fc38711e6
cmagister <paul.mcelhany@noaa.gov>
2019-08-08 15:47:11 -0700

    Added some code to loop through files. the code should really go in the getCreationDate.R file

--- fileRename.R
+++ fileRename.R
@@ -4,12 +4,29 @@ setwd("C:/Users/OALab/Documents/Juvenile photos/practice")
 #List all files in that directory
 file_list <- list.files (pattern=".jpg")
 
-#Set working directory
-currentpath <- "C:/Users/OALab/Documents/Juvenile photos/practice"
+#Set working directory path to juvenile photos directory
+setwd("/Users/paul.mcelhany/Downloads/juv phots")
+#get the name and path to all the folders in the juv photos directory
+#these are the tank groups (e.g. "HA", "HB", etc.)
+tankGroupFolder <- list.dirs(full.names = TRUE, recursive = FALSE)
 #targetpath  <- "C:/Users/OALab/Documents/Juvenile photos/All Photos"
 
+#loop through all the tank groups
+for(i in 1:length(tankGroupFolder)){
+  #get the name and path to all the molt folder in the tank group folder
+  moltFolder <- list.dirs(path = tankGroupFolder[i],full.names = TRUE, recursive = FALSE)
+  #loop through all the molt folders in a given tank group
+  for(j in 1:length(moltFolder)){
+    #get at list of all the file names in the molt folder
+    file_list <- list.files(path = moltFolder[j], pattern=".jpg")
+  }
+  
+}
+
+
+
 #List all files in that directory
-file_list <- list.files (pattern=".jpg")
+file_list <- list.files(pattern=".jpg")
 
 for(i in 1:length(file_list)){
   setwd(currentpath)
